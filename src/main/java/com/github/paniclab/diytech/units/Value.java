871bab002b40082493d61b405f3e3c2cd3f4580a
panic <PanicLabLimited@gmail.com>
2023-05-28 10:50:13 +0300

    add dimensionless Value type

---
+++ src/main/java/com/github/paniclab/diytech/units/Value.java
@@ -0,0 +1,57 @@
+package com.github.paniclab.diytech.units;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+
+public final class Value {
+    private final BigDecimal value;
+
+    private Value(@NotNull BigDecimal value) {
+        this.value = value;
+    }
+
+    @NotNull
+    public static Value of(@NotNull BigDecimal value) {
+        return new Value(value);
+    }
+
+    @NotNull
+    public static Value of(int value) {
+        return new Value(BigDecimal.valueOf(value));
+    }
+
+    @NotNull
+    public static Value of(long value) {
+        return new Value(BigDecimal.valueOf(value));
+    }
+
+    @NotNull
+    public static Value of(double value) {
+        return new Value(BigDecimal.valueOf(value));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (obj == this) return true;
+
+        if (obj instanceof Value other) {
+            return this.value.equals(other.value);
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Value(" + value + ")";
+    }
+}
