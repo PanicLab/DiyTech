ee40a1a81e47d1bb8aa8e7e360e71e7da9913e92
panic <PanicLabLimited@gmail.com>
2023-05-29 11:02:20 +0300

    add Width

---
+++ src/main/java/com/github/paniclab/diytech/dimention/Width.java
@@ -0,0 +1,65 @@
+package com.github.paniclab.diytech.dimention;
+
+import com.github.paniclab.diytech.units.LinearUnit;
+import com.github.paniclab.diytech.units.Value;
+import org.jetbrains.annotations.NotNull;
+
+import java.math.BigDecimal;
+import java.util.function.Function;
+
+
+public final class Width<U extends LinearUnit<U>> extends Dimension<Width<U>, U> {
+    private final U unit;
+    private final Class<U> unitType;
+
+    public Width(@NotNull U unit) {
+        this.unit = unit;
+        unitType = unit.getUnitType();
+    }
+
+    @NotNull
+    public static <X extends LinearUnit<X>> Width<X> valueOf(BigDecimal value, Function<BigDecimal, X> supplier) {
+        return new Width<>(supplier.apply(value));
+    }
+
+    @NotNull
+    public static <X extends LinearUnit<X>> Width<X> valueOf(int value, Function<BigDecimal, X> supplier) {
+        return new Width<>(supplier.apply(BigDecimal.valueOf(value)));
+    }
+
+    @NotNull
+    public static <X extends LinearUnit<X>> Width<X> valueOf(long value, Function<BigDecimal, X> supplier) {
+        return new Width<>(supplier.apply(BigDecimal.valueOf(value)));
+    }
+
+    @NotNull
+    public static <X extends LinearUnit<X>> Width<X> valueOf(double value, Function<BigDecimal, X> supplier) {
+        return new Width<>(supplier.apply(BigDecimal.valueOf(value)));
+    }
+
+
+    @Override
+    public @NotNull U value() {
+        return this.unit;
+    }
+
+    @Override
+    public @NotNull Width<U> add(@NotNull Width<U> other) {
+        return new Width<>(value().add(other.value()));
+    }
+
+    @Override
+    public @NotNull Dimension<Width<U>, U> multiply(@NotNull Value value) {
+        return new Width<>(value().multiply(value));
+    }
+
+    @Override
+    public @NotNull Dimension<Width<U>, U> divide(@NotNull Value value) {
+        return new Width<>(value().divide(value));
+    }
+
+    @Override
+    public String toString() {
+        return "Ширина(" + unit + ")";
+    }
+}
